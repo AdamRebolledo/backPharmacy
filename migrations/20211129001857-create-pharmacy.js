6b7402812da8dd4c52df0b14a338b89739024493
AdamRebolledo <adam.rebolledo@gmail.com>
2021-12-01 08:56:50 -0300

    feat: user crud

--- migrations/20211129001857-create-pharmacy.js
+++ migrations/20211129001857-create-pharmacy.js
@@ -11,6 +11,9 @@ module.exports = {
       locale_id: {
         type: Sequelize.STRING
       },
+      user_id: {
+        type: Sequelize.STRING
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
--- server.js
+++ server.js
@@ -31,10 +31,30 @@ app.get('/', (req, res) => {
   res.send("Hello Worlderferfer");
 });
 
-app.post('/api/v1/users/login', (req, res) => {
+app.post('/api/v1/users/login', async (req, res) => {
 
-  console.log(req.body)
-  res.send('Data received');
+  const user = await models.user.findAll({
+    where: {
+      email: req.body.email,
+      password: req.body.password
+    }
+  }).then((response) => {
+    if (response?.[0]?.dataValues?.email === undefined) {
+      res.json({
+        "message": 'Email o contraseña no se coinciden o el usuario no se encuentra registrado'
+      });
+    } else {
+      res.json({
+        "message": response?.[0]?.dataValues?.email
+      });
+    }
+
+  }).catch((err) => {
+    console.log(err)
+    res.json({
+      "message": err?.errors?.[0]?.message
+    });
+  });
 })
 
 app.get('/api/v1/users', async (req, res) => {
@@ -60,15 +80,60 @@ app.delete('/api/v1/users/delete/:id', async (req, res) => {
   res.send('200')
 });
 
-app.post('/api/v1/users/create', async (req, res) => {
-  const user = await models.user.create({
+app.post('/api/v1/users/create', (req, res) => {
+  const user = models.user.create({
     email: req.body.email,
     password: req.body.password
+  }).then((response) => {
+    sendEmail(req.body.email)
+    res.json({
+      "message": "Usuario creado correctamente"
+    });
+  }).catch((err) => {
+    res.json({
+      "message": err?.errors?.[0]?.message
+    });
   });
-  sendEmail(req.body.email)
-  res.send(user)
 })
 
+app.post('/api/v1/users/changepass', async (req, res) => {
+  const user = await models.user.findAll({
+    where: {
+      email: req.body.email,
+    }
+  }).then((response) => {
+    if (response?.[0]?.dataValues?.email === undefined) {
+      res.json({
+        "message": 'Email no se encuentra registrado, favor registrarse'
+      });
+    } else {
+      res.json({
+        "message": response?.[0]?.dataValues?.email
+      });
+    }
+
+  }).catch((err) => {
+    console.log(err)
+    res.json({
+      "message": err?.errors?.[0]?.message
+    });
+  });
+})
+app.post('/api/v1/users/confirmpass', async (req, res) => {
+  console.log(req.body)
+  const user = await models.user.update(req.body, {
+    where: { email: req.body.email }
+  }).then((response) => {
+    res.json({
+      "message": "Contraseña actualizada correctamente"
+    });
+  }).catch((err) => {
+    console.log(err)
+    res.json({
+      "message": err?.errors?.[0]?.message
+    });
+  });
+})
 app.post('/api/v1/users/update', async (req, res) => {
   const user = await models.user.update(req.body, {
     where: { email: req.body.email }
